6f65c6a74722404b8a5b1ecfea90910854691a39
GreenGatorade <1benhaak@gmail.com>
2026-03-06 09:16:12 -0800

    Configure server port and initialize messages.json
    Ensure the port is set as an integer and bind to all interfaces. Create messages.json if it doesn't exist.

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'json'
-set :port, ENV['PORT'] || 4567
+
+set :port, (ENV['PORT'] || 4567).to_i
+set :bind, '0.0.0.0'
+
+# Create messages.json if it doesn't exist yet
+File.write('messages.json', '[]') unless File.exist?('messages.json')
 
 get '/' do
   @messages = JSON.parse(File.read('messages.json')) rescue []
