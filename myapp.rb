e2931fae6d2a0d3dc535df14324bfe46a293f5a5
GreenGatorade <1benhaak@gmail.com>
2026-03-06 21:13:32 -0800

    Add protection setting to Sinatra app

--- myapp.rb
+++ myapp.rb
@@ -3,6 +3,7 @@ require 'json'
 
 set :port, (ENV['PORT'] || 4567).to_i
 set :bind, '0.0.0.0'
+set :protection, except: :host_authorization
 
 # Create messages.json if it doesn't exist yet
 File.write('messages.json', '[]') unless File.exist?('messages.json')
