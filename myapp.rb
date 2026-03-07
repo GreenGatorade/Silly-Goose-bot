3857435f46cc66e6c9c9399588032d6cdd3f3afb
GreenGatorade <1benhaak@gmail.com>
2026-03-06 23:14:12 -0800

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -5,6 +5,9 @@ set :port, (ENV['PORT'] || 4567).to_i
 set :bind, '0.0.0.0'
 set :protection, false
 
+# Allow all hosts (needed for Railway's internal proxy)
+set :host_authorization, { permitted_hosts: [] }
+
 # Create messages.json if it doesn't exist yet
 File.write('messages.json', '[]') unless File.exist?('messages.json')
 
